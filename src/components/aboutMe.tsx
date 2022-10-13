ec6ea99ba962b388dbf891494d891bec1eaf6e60
SantinoFajardo <santinofajardo02@gmail.com>
2022-10-12 22:57:50 -0300

    aboutMe

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -21,8 +21,13 @@ const AboutMe: NextPage = () => {
             : traductionOfAboutMe.firstParagraph.EN}
           </p>
         </div>
+        <div className={s.vline}>
+      </div>
       <div className={s.firstImgDiv}>
         <img src="https://mirazarate.com.ar/wp-content/uploads/2019/05/cnz-1-696x392.jpeg" alt="" className={s.firstImage}/>
+      </div>
+      <div className={s.secondImgDiv}>
+        <img src="https://avatars.githubusercontent.com/u/106558763?v=4" alt="" className={s.secondImage}/>
       </div>
       <div className={s.secondParagraphDiv}>
           <p className={s.secondParagraph}>
