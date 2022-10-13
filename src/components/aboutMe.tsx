d02e3c099ada6d5109091f089df05a16905a20bf
SantinoFajardo <santinofajardo02@gmail.com>
2022-10-12 22:28:33 -0300

    aboutMe first changes

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,17 +1,30 @@
 import { NextPage } from "next";
 import traductionOfAboutMe from "../lenguajes/AboutMe";
 import Cookies from "universal-cookie";
+import s from '../styles/aboutMe.module.css'
 
 const AboutMe: NextPage = () => {
   const Cookie = new Cookies();
   let lenguajeofPage = Cookie.get("Lenguaje");
   return (
-    <div>
-      <h1>
+    <div className={s.divAboutMe}>
+      <h1 className={s.aboutMeTitle}>
         {lenguajeofPage == "ES"
           ? traductionOfAboutMe.title.ES
           : traductionOfAboutMe.title.EN}
       </h1>
+      <div className={s.aboutMeFlexDiv}>
+        <div className={s.firstParagraphDiv}>
+          <p className={s.firstParagraph}>
+            {lenguajeofPage == "ES"
+            ? traductionOfAboutMe.firstParagraph.ES
+            : traductionOfAboutMe.firstParagraph.EN}
+          </p>
+        </div>
+      <div className={s.firstImgDiv}>
+        <img src="https://mirazarate.com.ar/wp-content/uploads/2019/05/cnz-1-696x392.jpeg" alt="" className={s.firstImage}/>
+      </div>
+      </div>
     </div>
   );
 };
--- src/lenguajes/AboutMe.ts
+++ src/lenguajes/AboutMe.ts
@@ -2,6 +2,10 @@ const traductionOfAboutMe = {
     title:{
         ES:"SOBRE MI",
         EN:"ABOUT ME"
+    },
+    firstParagraph:{
+        ES: "Hola! Mi nombre es Santino, soy un apasionado a la programacion por lo grandes retos que debo afrontar dia. Me encanta ponerme a prueba todos los dias con challenges que me voy encontrando. Me gusta leer muchos libros de superacion personal y grupal o entrenar en mis tiempos libres, me ayuda a desconectarme y poder cargar pilas. Suelo destacarme un mi organizacion y la discplina que logre inculcar en mis años como deportista de alto rendimiento. Me considero un programador full-stack, no solo por mis conocimientos en ambos campos, sino tambien por mi capacidad de poder desempeñarme en los dos sectores con fluidez. En todos los proyectos donde tuve la oportunidad de estar presente, participe tanto en el back como en el front.",
+        EN: "Hello! My name is Santino, I am passionate about programming because of the great challenges I have to face every day. I love putting myself to the test every day with challenges that I find. I like to read many books on personal and group improvement or training in my free time, it helps me disconnect and be able to recharge my batteries. I usually stand out for my organization and the discipline that I managed to instill in my years as a high-performance athlete. I consider myself a full-stack programmer, not only because of my knowledge in both fields, but also because of my ability to perform fluently in both sectors. In all the projects where I had the opportunity to be present, I participated both in the back and in the front."
     }
 }
 
