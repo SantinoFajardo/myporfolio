a643b421343455a7531ebbffdfc45fd450b4d32e
SantinoFajardo <santinofajardo02@gmail.com>
2022-10-14 18:23:07 -0300

    vline in skills section

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import traductionOfAboutMe from "../lenguajes/AboutMe";
 import Cookies from "universal-cookie";
-import s from '../styles/aboutMe.module.css'
+import s from "../styles/aboutMe.module.css";
 
 const AboutMe: NextPage = () => {
   const Cookie = new Cookies();
@@ -17,23 +17,30 @@ const AboutMe: NextPage = () => {
         <div className={s.firstParagraphDiv}>
           <p className={s.firstParagraph}>
             {lenguajeofPage == "ES"
-            ? traductionOfAboutMe.firstParagraph.ES
-            : traductionOfAboutMe.firstParagraph.EN}
+              ? traductionOfAboutMe.firstParagraph.ES
+              : traductionOfAboutMe.firstParagraph.EN}
           </p>
         </div>
-        <div className={s.vline}>
-      </div>
-      <div className={s.firstImgDiv}>
-        <img src="https://mirazarate.com.ar/wp-content/uploads/2019/05/cnz-1-696x392.jpeg" alt="" className={s.firstImage}/>
-      </div>
-      <div className={s.secondImgDiv}>
-        <img src="https://avatars.githubusercontent.com/u/106558763?v=4" alt="" className={s.secondImage}/>
-      </div>
-      <div className={s.secondParagraphDiv}>
+        <div className={s.vline}></div>
+        <div className={s.firstImgDiv}>
+          <img
+            src="https://mirazarate.com.ar/wp-content/uploads/2019/05/cnz-1-696x392.jpeg"
+            alt=""
+            className={s.firstImage}
+          />
+        </div>
+        <div className={s.secondImgDiv}>
+          <img
+            src="https://avatars.githubusercontent.com/u/106558763?v=4"
+            alt=""
+            className={s.secondImage}
+          />
+        </div>
+        <div className={s.secondParagraphDiv}>
           <p className={s.secondParagraph}>
             {lenguajeofPage == "ES"
-            ? traductionOfAboutMe.secondParagraph.ES
-            : traductionOfAboutMe.secondParagraph.EN}
+              ? traductionOfAboutMe.secondParagraph.ES
+              : traductionOfAboutMe.secondParagraph.EN}
           </p>
         </div>
       </div>
--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,81 +1,68 @@
 import { NextPage } from "next";
-import s from '../styles/skills.module.css'
+import s from "../styles/skills.module.css";
 import skillsTraduction from "../lenguajes/skills";
 import Cookies from "universal-cookie";
 import technologiesLink from "../utils/technologiesIcons";
 
-const Skills : NextPage = () => {
-    const Cookie = new Cookies()
-    let lenguajeofPage : string = Cookie.get("Lenguaje");
-    return(
-        <div id="skills" className={s.skillsDiv}>
-            <h1 className={s.skillsTitle}>{
-            lenguajeofPage == "ES" 
-            ? skillsTraduction.title.ES 
-            : skillsTraduction.title.EN
-            }</h1>
-            <div className={s.divIcons}>
-                <div className={s.flexDivSkills}>
-                    <h2 className={s.titleOfTechnologieType}>
-                        {
-                            lenguajeofPage == "ES"
-                            ? skillsTraduction.front.ES
-                            : skillsTraduction.front.EN
-                        }
-                    </h2>
-                <div className={s.divIconsFront}>
-                    {
-                        technologiesLink.Front.map((FL:string)=>(
-                            <img src={FL} width="70px" height="70px" alt="" />
-                        ))
-                    }
-                </div>
-                </div>
-                <div className={s.flexDivSkills}>
-                    <h2 className={s.titleOfTechnologieType}>BACKEND</h2>
-                <div className={s.divIconsFront}>
-                    {
-                        technologiesLink.Back.map((FL:string)=>(
-                            <img src={FL} width="70px" height="70px" alt="" />
-                        ))
-                    }
-                </div>
-                </div>
-                <div className={s.flexDivSkills}>
-                    <h2 className={s.titleOfTechnologieType}>
-                        {
-                            lenguajeofPage == "ES"
-                            ? skillsTraduction.dataBases.ES
-                            : skillsTraduction.dataBases.EN
-                        }
-                    </h2>
-                <div className={s.divIconsFront}>
-                    {
-                        technologiesLink.DataBases.map((FL:string)=>(
-                            <img src={FL} width="70px" height="70px" alt="" />
-                        ))
-                    }
-                </div>
-                </div>
-                <div className={s.flexDivSkills}>
-                    <h2 className={s.titleOfTechnologieType}>
-                        {
-                            lenguajeofPage == "ES"
-                            ? skillsTraduction.others.ES
-                            : skillsTraduction.others.EN
-                        }
-                    </h2>
-                <div className={s.divIconsFront}>
-                    {
-                        technologiesLink.Others.map((FL:string)=>(
-                            <img src={FL} width="70px" height="70px" alt="" />
-                        ))
-                    }
-                </div>
-                </div>
-            </div>
+const Skills: NextPage = () => {
+  const Cookie = new Cookies();
+  let lenguajeofPage: string = Cookie.get("Lenguaje");
+  return (
+    <div id="skills" className={s.skillsDiv}>
+      <h1 className={s.skillsTitle}>
+        {lenguajeofPage == "ES"
+          ? skillsTraduction.title.ES
+          : skillsTraduction.title.EN}
+      </h1>
+      <div className={s.vline}></div>
+      <div className={s.divIcons}>
+        <div className={s.flexDivSkills}>
+          <h2 className={s.titleOfTechnologieType}>
+            {lenguajeofPage == "ES"
+              ? skillsTraduction.front.ES
+              : skillsTraduction.front.EN}
+          </h2>
+          <div className={s.divIconsFront}>
+            {technologiesLink.Front.map((FL: string) => (
+              <img src={FL} width="70px" height="70px" alt="" />
+            ))}
+          </div>
         </div>
-    )
-}
+        <div className={s.flexDivSkills}>
+          <h2 className={s.titleOfTechnologieType}>BACKEND</h2>
+          <div className={s.divIconsFront}>
+            {technologiesLink.Back.map((FL: string) => (
+              <img src={FL} width="70px" height="70px" alt="" />
+            ))}
+          </div>
+        </div>
+        <div className={s.flexDivSkills}>
+          <h2 className={s.titleOfTechnologieType}>
+            {lenguajeofPage == "ES"
+              ? skillsTraduction.dataBases.ES
+              : skillsTraduction.dataBases.EN}
+          </h2>
+          <div className={s.divIconsFront}>
+            {technologiesLink.DataBases.map((FL: string) => (
+              <img src={FL} width="70px" height="70px" alt="" />
+            ))}
+          </div>
+        </div>
+        <div className={s.flexDivSkills}>
+          <h2 className={s.titleOfTechnologieType}>
+            {lenguajeofPage == "ES"
+              ? skillsTraduction.others.ES
+              : skillsTraduction.others.EN}
+          </h2>
+          <div className={s.divIconsFront}>
+            {technologiesLink.Others.map((FL: string) => (
+              <img src={FL} width="70px" height="70px" alt="" />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
