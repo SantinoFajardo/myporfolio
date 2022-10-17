923c58b2c63caa4c7d96a91a5d2623720acc9a69
SantinoFajardo <santinofajardo02@gmail.com>
2022-10-17 13:02:25 -0300

    contact section

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -1,13 +1,18 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import Cookies from "universal-cookie";
 import s from "../styles/contact.module.css";
 import iconsContact from "../utils/contactIcons";
 import { CONTACT } from "../utils/interfaces";
 
 const Contact: NextPage = () => {
+  const Cookie = new Cookies();
+  let lenguageOfPage = Cookie.get("Lenguaje");
   return (
     <div className={s.contactContainer}>
-      <h1 className={s.contactTitle}>Contact me</h1>
+      <h1 className={s.contactTitle}>
+        {lenguageOfPage == "ES" ? "CONTACTAME" : "CONTACT ME"}
+      </h1>
       <div className={s.flexDivContact}>
         {iconsContact.map((IC: CONTACT, i: number) => (
           <Link href={IC.link} passHref>
