bbccc1b2a77a7087c1a9958ca2ff60f6f41b7949
SantinoFajardo <santinofajardo02@gmail.com>
2022-10-17 12:17:33 -0300

    contact section bases

--- src/components/contactMe.tsx
+++ src/components/contactMe.tsx
@@ -1,7 +1,12 @@
 import { NextPage } from "next";
+import s from "../styles/contact.module.css";
 
 const Contact: NextPage = () => {
-  return <div></div>;
+  return (
+    <div className={s.contactontainer}>
+      <h1 className={s.contactTitle}>ContactMe</h1>
+    </div>
+  );
 };
 
 export default Contact;
